13b869c8425161d32b8077ca31dc7f47793b5691
Devon Vendetti <v3nd3tti@icloud.com>
2022-03-15 00:58:32 -0700

    add padding to some areas for mobile

--- script/script.js
+++ script/script.js
@@ -5,7 +5,18 @@
 // });
 const menuToggle = document.querySelector('.menu-toggle');
 const sidebar = document.querySelector('.sidebar');
+const form = document.querySelector('#contact');
 menuToggle === null || menuToggle === void 0 ? void 0 : menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('is-active');
     sidebar === null || sidebar === void 0 ? void 0 : sidebar.classList.toggle('is-active');
+});
+form === null || form === void 0 ? void 0 : form.addEventListener('sumbit', (e) => {
+    e.preventDefault();
+    const email = document.querySelector('#email').value;
+    console.log(email);
+    const name = document.querySelector('#name').value;
+    console.log(name);
+    const msg = document.querySelector('#comment').value;
+    console.log(name);
+    form.reset();
 });
